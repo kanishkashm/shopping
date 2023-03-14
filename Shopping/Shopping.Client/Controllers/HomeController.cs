346dba836e40a2fa1cb881f1d39576cbe44e5e4d
Kanishka <kanishkashm@gmail.com>
2023-03-15 02:06:09 +0530

    first commit

---
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shopping.Client.Models;
+using System.Diagnostics;
+
+namespace Shopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(
+            IHttpClientFactory httpClientFactory,
+            ILogger<HomeController> logger
+        )
+        {
+            _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("api/Product");
+            var content = await response.Content.ReadAsStringAsync();
+            var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+            return View(productList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
